aae4835f3ed37a48a313e54cda1bab998f465dfd
Rudi B. Svartveit <rudsvar@gmail.com>
2019-12-12 10:47:16 +0100

    Simplify main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate image;
 
-use image::RgbImage;
 use image::Rgb;
+use image::RgbImage;
 
 use std::env::args;
 use std::process::exit;
@@ -20,16 +20,16 @@ fn main() {
             let default = 0.01;
             println!("Invalid scale, defaulting to {}", default);
             default
-        },
+        }
     };
 
     match image::open(&args[1]) {
         Ok(img) => {
             let img = img.to_rgb();
-            let chars = ".;+%#".chars().collect();
+            let chars = "#%+;.".chars().collect();
             let ascii = to_ascii(img, scale, &chars);
             println!("{}", ascii);
-        },
+        }
         Err(e) => println!("Could not open image: {}", e),
     };
 }
@@ -39,18 +39,18 @@ fn to_ascii(img: RgbImage, scale: f64, chars: &Vec<char>) -> String {
     res.push_str("\x1b[7l");
 
     let (w, h) = img.dimensions();
-    let step_size = (1.0 / scale) as u32;
+    let step_size = (1.0 / scale) as usize;
 
-    for y in range(0, h, step_size) {
-        for x in range(0, w, step_size) {
-            let px = img.get_pixel(x,y);
+    for y in (0..h).step_by(step_size) {
+        for x in (0..w).step_by(step_size) {
+            let px = img.get_pixel(x, y);
             let (r, g, b) = rgb(&px);
-            let avg = ((r+g+b)/3) as f64;
+            let avg = ((r + g + b) / 3) as f64;
             let c = num_to_char(avg, chars);
             res.push_str(&format!("\x1b[38;2;{};{};{}m,{}", r, g, b, c));
 
-            let percent = (y*w + x) as f32 / (w*h) as f32;
-            print!("{:.1}%\r", percent*100.0);
+            let percent = (y * w + x) as f32 / (w * h) as f32;
+            print!("{:.1}%\r", percent * 100.0);
         }
         res.push('\n');
     }
@@ -59,19 +59,13 @@ fn to_ascii(img: RgbImage, scale: f64, chars: &Vec<char>) -> String {
     return res;
 }
 
-fn range(start: u32, end: u32, sz: u32) -> Vec<u32> {
-    (start .. end)
-        .filter(|x| x % sz == 0)
-        .collect()
-}
-
-fn rgb(px: &Rgb<u8>) -> (i32, i32, i32) {
-    (px[0] as i32, px[1] as i32, px[2] as i32)
+fn rgb(px: &Rgb<u8>) -> (u8, u8, u8) {
+    (px[0], px[1], px[2])
 }
 
 fn num_to_char(avg: f64, chars: &Vec<char>) -> char {
-    let ratio = avg/256.0;
-    let len = chars.len() as f64;
-    let index = ratio * len;
+    let ratio = avg / 256.0;
+    let len = chars.len();
+    let index = ratio * len as f64;
     chars[index as usize]
 }
